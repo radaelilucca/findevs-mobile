172dad1b6f72e43935a4b6a9f14d245973d5b29a
Lucca Radaeli <radaelidev@MacBook-Pro-de-Lucca.local>
2020-01-22 14:22:39 -0800

    Update assets

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import socketio from 'socket.io-client'
 
-const socket = socketio('http://192.168.0.37:3333', {
+const socket = socketio('https://findev-omniweek.herokuapp.com/', {
   autoConnect: false,
 })
 
